31133e1256663032717959c9d5f1e10d0b279c0f
luissevilla <luissevilla111@gmail.com>
2022-05-05 19:39:19 -0500

    reading and converting to json excel

--- app.ts
+++ app.ts
@@ -1,4 +1,23 @@
 import { emailUSer } from "./enviroment/variables";
 
+import XLSX from "xlsx";
+import { TypeDate } from "./interfaces/dates";
 
+const ruta = "./data/tipocitas.xls";
 
+const getTipoCitasFromXlsx = (fileRoot: string) => {
+  try {
+    const workbook = XLSX.readFile(fileRoot);
+    const workbookSheet = workbook.SheetNames;
+    //console.log(workbookSheet);
+    const sheet = workbookSheet[0];
+    const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+    const typeDatesArray = dataExcel as TypeDate[];
+    return typeDatesArray;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+const calendarsZoho = getTipoCitasFromXlsx(ruta);
---
+++ interfaces/dates.ts
@@ -0,0 +1,7 @@
+export interface TypeDate {
+  "Tipo de Cita": string;
+  Nombre: string;
+  "Tipo Evento": string;
+  Calendar_ID: string;
+  ID: string;
+}
